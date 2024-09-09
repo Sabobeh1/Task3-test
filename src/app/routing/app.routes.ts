480ee10a28a7999059edcadab0a6f88c3ba7da46
Sabobeh1 <146986675+Sabobeh1@users.noreply.github.com>
2024-09-09 12:40:05 +0300

    brief edits

--- src/app/routing/app.routes.ts
+++ src/app/routing/app.routes.ts
@@ -3,6 +3,8 @@ import { UserListComponent } from '../components/user-list/user-list.component';
 import { UserDetailsComponent } from '../components/user-details/user-details.component';
 
 export const routes: Routes = [
-  { path: '', component: UserListComponent },
+  { path: 'users', component: UserListComponent },
   { path: 'users/:id', component: UserDetailsComponent },
+  { path: '', redirectTo: 'users', pathMatch: 'full' },  // Default redirect
+
 ];
